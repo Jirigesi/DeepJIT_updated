1e2f379a36c652b640e585736a9dec66d8aed37f
fjirigesi <fjirigesi@ebay.com>
2021-04-13 21:02:29 -0700

    update siamese model

--- siamese.py
+++ siamese.py
@@ -35,12 +35,12 @@ class DeepJITSiamese(nn.Module):
     def forward_msg(self, x, convs):
         # note that we can use this function for commit code line to get the information of the line\
         # print("x shape[0], [1]: ", x.shape[0], x.shape[1], x.shape[2], x.shape[0])
-        x = x.unsqueeze(1)  # (N, Ci, W, D)
+        # x = x.unsqueeze(1)  # (N, Ci, W, D)
         print("x type:", type(x), x.shape)
 
-        # jiri made change here from 3 to 1
+        #
         x = [F.relu(conv(x)).squeeze(3) for conv in convs]  # [(N, Co, W), ...]*len(Ks)
-        # jiri made change here from 2 to 1
+        #
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N, Co), ...]*len(Ks)
 
         x = torch.cat(x, 1)
