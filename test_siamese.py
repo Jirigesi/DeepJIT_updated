ee50ec8837e1f2fa59ddeeb10eb71f0bda374e8b
fjirigesi <fjirigesi@ebay.com>
2021-04-14 17:30:41 -0700

    update siamese model validation

--- test_siamese.py
+++ test_siamese.py
@@ -39,9 +39,9 @@ def evaluation_siamese_model(data, all_bug_data, params):
     with torch.no_grad():
         all_predict, all_label = list(), list()
 
-        for j, compare_batch in enumerate(compare_batches):
+        for i, batch in enumerate(batches):
 
-            for i, batch in enumerate(batches):
+            for j, compare_batch in enumerate(compare_batches):
                 distances = []
                 print("batches times", i)
                 print("compartive times:", j)
