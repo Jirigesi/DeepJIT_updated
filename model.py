98f6ee94d1ebb07d4a79f02d5902b3115adb00ab
fjirigesi <fjirigesi@ebay.com>
2021-04-19 15:27:32 -0700

    update siamese model validation

--- model.py
+++ model.py
@@ -42,7 +42,7 @@ class DeepJIT(nn.Module):
 
     def forward_code(self, x, convs_line, convs_hunks):
 
-        print("x shape[0], [1]: ", x.shape[0], x.shape[1], x.shape[2], x.shape[3])
+        # print("x shape[0], [1]: ", x.shape[0], x.shape[1], x.shape[2], x.shape[3])
 
         n_batch, n_file = x.shape[0], x.shape[1]
         x = x.reshape(n_batch * n_file, x.shape[2], x.shape[3])
