4bf5b1627702c6c5aed6112c46d4629d1ce28bc0
fjirigesi <fjirigesi@ebay.com>
2021-04-23 12:49:29 -0700

    update siamese model validation

--- runSiameseEvaluation.py
+++ runSiameseEvaluation.py
@@ -4,5 +4,5 @@ import subprocess
 for number in range(10, 16):
 
     cmd = "python main.py -predict -pred_data ./splittedData/hard_Filecount_openstack_test.pkl -buggy_data ./data/train_buggy.pkl -dictionary_data" \
-          " ../deepjit_os/openstack_dict.pkl -load_model ./snapshot/2021-04-22_11-22-56/epoch_" +number +".pt "
+          " ../deepjit_os/openstack_dict.pkl -load_model ./snapshot/2021-04-22_11-22-56/epoch_" + str(number) +".pt "
     subprocess.run(cmd)
