671e389568a7c6a59ef4c5e5753c429be600b549
fjirigesi <fjirigesi@ebay.com>
2021-04-23 12:55:46 -0700

    update siamese model validation

--- runSiameseEvaluation.py
+++ runSiameseEvaluation.py
@@ -1,8 +1,13 @@
 import subprocess
 
+import time
 
+model_path = "./snapshot/2021-04-22_11-22-56/epoch_"
+test_File_path = "./splittedData/hard_Filecount_openstack_test.pkl "
 for number in range(10, 16):
 
-    cmd = "python main.py -predict -pred_data ./splittedData/hard_Filecount_openstack_test.pkl -buggy_data ./data/train_buggy.pkl -dictionary_data" \
-          " ../deepjit_os/openstack_dict.pkl -load_model ./snapshot/2021-04-22_11-22-56/epoch_" + str(number) +".pt "
-    subprocess.check_output(cmd, shell = True)
+    cmd = "python main.py -predict -pred_data " + test_File_path +"-buggy_data ./data/train_buggy.pkl -dictionary_data" \
+          " ../deepjit_os/openstack_dict.pkl -load_model " + model_path + str(number) + ".pt"
+    # Wait for 5 seconds
+    time.sleep(5)
+    subprocess.check_output(cmd, shell=True)
