e50009e69d3ece1e7eeb8065d973226800ba4401
fjirigesi <fjirigesi@ebay.com>
2021-04-23 12:51:31 -0700

    update siamese model validation

--- runSiameseEvaluation.py
+++ runSiameseEvaluation.py
@@ -5,4 +5,4 @@ for number in range(10, 16):
 
     cmd = "python main.py -predict -pred_data ./splittedData/hard_Filecount_openstack_test.pkl -buggy_data ./data/train_buggy.pkl -dictionary_data" \
           " ../deepjit_os/openstack_dict.pkl -load_model ./snapshot/2021-04-22_11-22-56/epoch_" + str(number) +".pt "
-    subprocess.run(cmd)
+    subprocess.check_output(cmd, shell = True)
