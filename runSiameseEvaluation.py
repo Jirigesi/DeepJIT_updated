2590dfecc17f6b3f85c1b35c20b72850532c4f09
fjirigesi <fjirigesi@ebay.com>
2021-05-09 22:50:06 -0700

    siamese data

--- runSiameseEvaluation.py
+++ runSiameseEvaluation.py
@@ -25,7 +25,7 @@ for test_File_path in test_File_path_list:
         for number in range(15, 26):
 
             cmd = "python main.py -predict -pred_data " + test_File_path + " " + "-buggy_data ./data/train_buggy.pkl -dictionary_data" \
-                  " ../deepjit_os/openstack_dict.pkl -load_model " + model_path + str(number) + ".pt"
+                  " qt_data/qt_dict.pkl -load_model " + model_path + str(number) + ".pt"
             # Wait for 5 seconds
 
             subprocess.check_output(cmd, shell=True)
