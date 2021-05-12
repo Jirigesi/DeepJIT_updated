cbc907d17f3cb1017cf1c9a9b445733e5a220ebc
fjirigesi <fjirigesi@ebay.com>
2021-05-12 11:11:31 -0700

    run siamese results

--- runSiameseEvaluation.py
+++ runSiameseEvaluation.py
@@ -10,7 +10,7 @@ import time
 # model_path_list = ["./snapshot/2021-04-23_15-13-25/epoch_", "./snapshot/2021-04-23_20-54-01/epoch_"]
 # model_path_list = ["./snapshot/2021-04-24_12-46-04/epoch_", "./snapshot/2021-04-24_15-08-57/epoch_",
 #                    "./snapshot/2021-04-24_19-31-41/epoch_", "./snapshot/2021-04-24_22-36-20/epoch_"]
-model_path_list = ["./snapshot/2021-05-11_17-00-37/epoch_"]
+model_path_list = ["./snapshot/2021-05-11_22-23-14/epoch_"]
 
 
 #
